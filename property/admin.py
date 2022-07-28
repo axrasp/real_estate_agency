74f1c9813d6824958c8c2846ed221f5bdb2d82ea
axrasp <aaraspopin@gmail.com>
2022-07-28 12:40:13 +0300

    PEP8 rules

--- property/admin.py
+++ property/admin.py
@@ -12,16 +12,20 @@ class OwnerFlatsInstanceInline(admin.TabularInline):
 class AuthorAdmin(admin.ModelAdmin):
     search_fields = ('address',
                      'town',
-                     'pk')
+                     'pk'
+                     )
     readonly_fields = ['created_at']
-    list_display = ('address', 'price',
+    list_display = ('address',
+                    'price',
                     'new_building',
                     'construction_year',
-                    'town')
+                    'town'
+                    )
     list_editable = ['new_building']
     list_filter = ('new_building',
                    'has_balcony',
-                   'rooms_number')
+                   'rooms_number'
+                   )
     raw_id_fields = ['liked_by']
     inlines = [OwnerFlatsInstanceInline]
 
